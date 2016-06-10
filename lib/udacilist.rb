d605f916bdeab186128a8e7bdf65b4f0fab9d177
Kevin <kevin.mayo@udacity.com>
2016-06-09 20:13:19 -0700

    Add errors

--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -7,12 +7,26 @@ class UdaciList
   end
   def add(type, description, options={})
     type = type.downcase
+    valid_priorities = ["low", "medium", "high"]
+    if options[:priority]
+      if !valid_priorities.include? options[:priority]
+        raise UdaciListErrors::InvalidPriorityValue, "Invalid priority #{options[:priority]}, choose from #{valid_priorities}"
+      end
+    end
+    original_length = @items.length
     @items.push TodoItem.new(description, options) if type == "todo"
     @items.push EventItem.new(description, options) if type == "event"
     @items.push LinkItem.new(description, options) if type == "link"
+    if (@items.length - original_length) < 1
+      raise UdaciListErrors::InvalidItemType, "Type '#{type}' not supported"
+    end
   end
   def delete(index)
-    @items.delete_at(index - 1)
+    if index > @items.length
+      raise UdaciListErrors::IndexExceedsListSize, "Attempted to delete item that is not in the list"
+    else
+      @items.delete_at(index - 1)
+    end
   end
   def all
     puts "-" * @title.length
